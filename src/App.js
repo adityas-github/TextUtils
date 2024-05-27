5582aa1493ccde584aa1716b6639538716c0fcd5
adityas-github <madityat004@gmail.com>
2024-05-27 11:56:42 +0530

    Small Change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import TextForm from "./components/TextForm";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils"/>
       <div className="container my-3">
-      {/* <TextForm heading="Enter the text to analyze below"></TextForm> */}
-      <About className="my-2" />
+      <TextForm heading="Enter the text to analyze below"></TextForm>
+      {/* <About className="my-2" /> */}
       </div>
     </>
   );
