b9b8ca389c008a7d9fe9ad542e919ee71944badd
Adi <madityat004@gmail.com>
2024-05-28 12:24:46 +0530

    feat: Add homepage URL and deploy script
    - Added "homepage" field to package.json with the URL for the project's homepage
    - Added "predeploy" and "deploy" scripts to package.json for deploying the project to GitHub Pages
    - Updated dependencies in package.json to include "gh-pages" and "react-router-dom"
    
    Closes #123

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -24,26 +25,34 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-      // document.body.style.color = "white";
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
-      // document.body.style.color = "black";
     }
   };
+
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm
-          showAlert={showAlert}
-          heading="Enter the text to analyze below"
-          mode={mode}
-        ></TextForm>
-        {/* <About className="my-2" /> */}
-      </div>
+      {/* <BrowserRouter> */}
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          {/* <Routes> */}
+            {/* <Route */}
+              {/* path="/"
+              element={ */}
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                />
+              {/* } */}
+            {/* /> */}
+            {/* <Route path="/about" element={<About />} /> */}
+          {/* </Routes> */}
+        </div>
+      {/* </BrowserRouter> */}
     </>
   );
 }
